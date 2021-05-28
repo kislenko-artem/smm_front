dc1fc2e0f2ba1d5419b7c7828ee9930cbef4d9f8
artem <artem@webart-tech.ru>
2021-05-28 22:33:15 +0300

    анализ страницы

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,74 +1,77 @@
 <template>
-    <div class="account-sidebar">
-        <h3>Выберите: </h3>
-        <ul>
-            <li>
-                <NuxtLink to="/account">Поиск аудитории</NuxtLink>
-           </li>
-<!--            <li class="members">-->
-<!--                <a href="">Участники группы</a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--                <a href="">География подписчиков</a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--                <a href="">Активность участников</a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--                <a href="">Пересечение групп</a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--                <a href="">Стена группы</a>-->
-<!--            </li>-->
-        </ul>
-    </div>
+  <div class="account-sidebar">
+    <h3>Выберите: </h3>
+    <ul>
+      <li>
+        <NuxtLink to="/account">Поиск аудитории</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/account/members">Участники группы</NuxtLink>
+      </li>
+      <!--            <li class="members">-->
+      <!--                <a href="">Участники группы</a>-->
+      <!--            </li>-->
+      <!--            <li>-->
+      <!--                <a href="">География подписчиков</a>-->
+      <!--            </li>-->
+      <!--            <li>-->
+      <!--                <a href="">Активность участников</a>-->
+      <!--            </li>-->
+      <!--            <li>-->
+      <!--                <a href="">Пересечение групп</a>-->
+      <!--            </li>-->
+      <!--            <li>-->
+      <!--                <a href="">Стена группы</a>-->
+      <!--            </li>-->
+    </ul>
+  </div>
 </template>
 
 <style>
-    .account-sidebar {
-        position: relative;
-        box-sizing: border-box;
-        width: 15%;
-        min-width: 200px;
-        padding: 5px 0 1px 0;
-        background-color: white;
-        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.25);
-        border-radius: 5px;
-        border: none;
-        min-height: 200px;
+.account-sidebar {
+  position: relative;
+  box-sizing: border-box;
+  width: 15%;
+  min-width: 200px;
+  padding: 5px 0 1px 0;
+  background-color: white;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  border: none;
+  min-height: 200px;
 
-    }
+}
 
-    .account-sidebar a {
-        cursor: pointer;
-        color: #337ab7;
-    }
+.account-sidebar a {
+  cursor: pointer;
+  color: #337ab7;
+}
 
-    .account-sidebar h3 {
-        font-family: Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        font-weight: bold;
-        color: #3F454B;
-        text-transform: none;
-        margin: 0;
-        padding: 10px 20px 10px 24px;
-    }
+.account-sidebar h3 {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+  color: #3F454B;
+  text-transform: none;
+  margin: 0;
+  padding: 10px 20px 10px 24px;
+}
 
-    .account-sidebar ul {
-        list-style-type: none;
-        padding: 0 0 0 24px;
-    }
+.account-sidebar ul {
+  list-style-type: none;
+  padding: 0 0 0 24px;
+}
 
-    .account-sidebar ul a {
-        text-decoration: none;
-        color: #457ea4;
-    }
+.account-sidebar ul a {
+  text-decoration: none;
+  color: #457ea4;
+}
 
-    .account-sidebar ul a:hover {
-        color: #3F454B;
-    }
+.account-sidebar ul a:hover {
+  color: #3F454B;
+}
 
-    .account-sidebar ul li.active a {
-        color: #3F454B;
-    }
+.account-sidebar ul li.active a {
+  color: #3F454B;
+}
 </style>
---
+++ pages/account/members.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="account-wrapper">
+    <Sidebar/>
+    <div id="content">
+      <div id="content-header">
+        <h3>Поиск аудитории</h3>
+      </div>
+      <div class="add_group">
+        <div>
+          <label for="search">ID сообщества, которое хотите проанализировать:</label>
+        </div>
+        <div>
+          <input type="text" value="" id="search" v-model.trim="search"/>
+          <button v-on:click="startSearch">Старт</button>
+        </div>
+      </div>
+      <div class="account-charts">
+        <div>
+          <h3>Возраст участников</h3>
+          <table>
+            <tr v-for="item in age_data">
+              <td>{{ item.name }}</td>
+              <td>{{ item.value }}</td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <h3>География участников</h3>
+          <table>
+            <tr v-for="item in geography_data">
+              <td>{{ item.name }}</td>
+              <td>{{ item.value }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'account',
+  data: () => {
+    return {
+      search: "",
+      age_data: [],
+      geography_data: [],
+      current_screen: "",
+    }
+  },
+  methods: {
+    startSearch() {
+      const self = this;
+      fetch("http://localhost:9999/v0/vk/stat/groups/" + this.search, {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        referrer: "", // или "" для того, чтобы не послать заголовок Referer,
+        // или URL с текущего источника
+        referrerPolicy: "no-referrer", // no-referrer, origin, same-origin...
+        mode: "no-cors", // same-origin, no-cors
+        credentials: "same-origin", // omit, include
+        cache: "default", // no-store, reload, no-cache, force-cache или only-if-cached
+        redirect: "follow", // manual, error
+        integrity: "", // контрольная сумма, например "sha256-abcdef1234567890"
+        keepalive: false, // true
+        signal: undefined, // AbortController, чтобы прервать запрос
+        window: window // null
+      }).then((response) => {
+        let interval = setInterval(() => {
+          fetch('http://localhost:9999/v0/vk/stat/groups/' + this.search + "?age=1")
+            .then((response) => {
+                return response.json();
+              }
+            )
+            .then((data) => {
+              if (data != undefined && data.length == 0) {
+                return
+              }
+              self.age_data = data;
+              clearInterval(interval);
+            });
+        }, 1000)
+
+        let intervalAge = setInterval(() => {
+          fetch('http://localhost:9999/v0/vk/stat/groups/' + this.search + "?geography=1")
+            .then((response) => {
+                return response.json();
+              }
+            )
+            .then((data) => {
+              if (data != undefined && data.length == 0) {
+                return
+              }
+              self.geography_data = data;
+              clearInterval(intervalAge);
+            });
+        }, 1000)
+      })
+    },
+  }
+}
+</script>
+
+<style>
+.account-content {
+  display: flex;
+}
+
+.account-right {
+  padding: 10px;
+}
+
+.account-charts {
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+}
+
+</style>
