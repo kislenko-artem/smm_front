9a784054ecff761766ee67b48048e115b5464bff
artem <artem@webart-tech.ru>
2023-01-29 09:10:20 +0300

    добавлен филиал

---
+++ pages/business/offices.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="account-wrapper">
+    <Sidebar/>
+    <div id="content">
+      <div id="content-header">
+        <h3>Управление Филиалами</h3>
+      </div>
+      <div class="categories-main">
+        <article>
+          <h4>Филиалы</h4>
+          <hr/>
+          <div class="manage-data">
+            <input type="text" value="" id="serviceModel" v-model.trim="serviceModel"/>
+            <button v-on:click="addService">Добавить</button>
+          </div>
+          <hr/>
+          <Grid
+            :heroes="services"
+            :columns="serviceColumns"
+            :filter-key="searchQuery"
+            :methodsList="methodsServiceList"
+          />
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'account',
+  data: () => {
+    return {
+      searchQuery: "",
+
+      services: [],
+      sourceModel: "",
+      serviceColumns: ["Название", "method:Удалить:id:delService"],
+      methodsServiceList: {},
+
+      types: [],
+      typeModel: "",
+      typeColumns: ["Название", "method:Удалить:id:delType"],
+      methodsTypeList: {},
+
+      serviceModel: "",
+    }
+  },
+  mounted() {
+    this.getCategories("services", this.services);
+    this.getCategories("types", this.types);
+    this.methodsServiceList["delService"] = this.delService;
+    this.methodsTypeList["delType"] = this.delType;
+  },
+  methods: {
+    addService() {
+      this.addCategory("services", this.services, this.serviceModel);
+    },
+    addType() {
+      this.addCategory("types", this.types, this.typeModel);
+    },
+    delService(id) {
+      this.delCategory("services", this.services, id);
+    },
+    delType(id) {
+      this.delCategory("types", this.types, id);
+    },
+
+    getCategories(alias, model) {
+      const self = this;
+      fetch(process.env.baseUrl + "/v0/business/offices/" + alias)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+
+          if (model === self.services) {
+            self.services = [];
+            for (let key in data.results) {
+              self.services.push({
+                "Название": data.results[key]["name"],
+                "id": data.results[key]["id"],
+              });
+            }
+          }
+
+          if (model === self.types) {
+            self.types = [];
+            for (let key in data.results) {
+              self.types.push({
+                "Название": data.results[key]["name"],
+                "id": data.results[key]["id"],
+              });
+            }
+          }
+
+
+
+        })
+    },
+    addCategory(alias, model, name) {
+      const self = this;
+      fetch(process.env.baseUrl + "/v0/business/offices/offices", {
+        method: 'POST',
+        body: JSON.stringify({name: name, category_type: alias})
+      })
+        .then((data) => {
+          self.getCategories(alias, model);
+        });
+    },
+    delCategory(alias, model, id) {
+      const self = this;
+      fetch(process.env.baseUrl + "/v0/business/offices/" + id, {
+        method: 'DELETE',
+      })
+        .then((data) => {
+          self.getCategories(alias, model);
+        });
+
+    }
+  }
+}
+</script>
+
+<style>
+.categories-main {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.categories-main article{
+  width: 50%;
+}
+.account-content {
+  display: flex;
+}
+
+.account-right {
+  padding: 10px;
+}
+
+.items-list {
+  cursor: pointer;
+}
+
+@media screen and (max-width: 450px) {
+  .categories-main article {
+    width: 100%;
+  }
+}
+
+</style>
